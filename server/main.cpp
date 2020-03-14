29c3d72163c123de26c84f1580aa60a6321c9d03
Hamza <muhammadhamza1311@gmail.com>
2020-03-14 20:12:53 +0500

    Add a basic websockets server

--- server/main.cpp
+++ server/main.cpp
@@ -1,6 +1,80 @@
-#include<iostream>
+#include <websocketpp/server.hpp>
+#include <websocketpp/config/asio_no_tls.hpp>
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+typedef websocketpp::server<websocketpp::config::asio> server;
+
+using websocketpp::lib::placeholders::_1;
+using websocketpp::lib::placeholders::_2;
+using websocketpp::lib::bind;
+
+
+// pull out the type of messages sent by our config
+typedef server::message_ptr message_ptr;
+
+// Define a callback to handle incoming messages
+void on_message(server *s, websocketpp::connection_hdl hdl, message_ptr msg) {
+
+    try {
+        std::string filename = "name.png"; // temp
+        std::string payload = msg->get_payload();
+        websocketpp::frame::opcode::value opcode = msg->get_opcode();
+
+        if (opcode == websocketpp::frame::opcode::text) {
+            std::cout << "Text Message Received\n";
+	    
+        } else if (opcode == websocketpp::frame::opcode::binary) {
+            std::cout << filename << "\n";
+            std::cout << "Size " << payload.size() << "\n";
+
+            std::ofstream outfile;
+            outfile.open(filename);
+            outfile << payload;
+            outfile.close();
+
+            std::cout << "\nBinary received\n";
+
+            s->send(hdl, payload, msg->get_opcode());
+        }
+
+    } catch (websocketpp::exception const &e) {
+        std::cout << "Echo failed because: "
+                  << "(" << e.what() << ")" << std::endl;
+    }
+}
 
 int main() {
     std::cout << "Hello World" << std::endl;
+    // Create a server endpoint
+    server echo_server;
+
+    try {
+        // Set logging settings
+        echo_server.set_access_channels(websocketpp::log::alevel::none);
+        echo_server.clear_access_channels(websocketpp::log::alevel::none);
+
+        // Initialize Asio
+        echo_server.init_asio();
+
+        // Register our message handler
+        echo_server.set_message_handler(bind(&on_message, &echo_server, ::_1, ::_2));
+
+        // Listen on port 9002
+        echo_server.listen(9002);
+
+        // Start the server accept loop
+        echo_server.start_accept();
+
+        // Start the ASIO io_service run loop
+        echo_server.run();
+    } catch (const std::exception& e) {
+        std::cout << e.what() << std::endl;
+    } catch (...) {
+        std::cout << "other exception" << std::endl;
+    }
+
     return 0;
 }
