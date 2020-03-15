3df67f6b7c4e3b43a129d3d03fc65a860c48afd7
Hamza <muhammadhamza1311@gmail.com>
2020-03-15 14:00:04 +0500

    code cleanup - don't put everything in one file

---
+++ server/impl/server.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <utility>
+
+#include <websocketpp/server.hpp>
+#include <websocketpp/config/asio_no_tls.hpp>
+
+typedef websocketpp::server<websocketpp::config::asio> asio_server;
+
+#include "headers/server.hpp"
+
+using websocketpp::lib::placeholders::_1;
+using websocketpp::lib::placeholders::_2;
+using websocketpp::lib::bind;
+
+using server::Server;
+
+Server::Server(int _port) {
+    this->port = _port;
+
+    server.set_access_channels(websocketpp::log::alevel::none);
+    server.clear_access_channels(websocketpp::log::alevel::none);
+    server.init_asio();
+
+    server.set_message_handler(bind(&Server::on_message, this, ::_1, ::_2));
+    server.set_open_handler(bind(&Server::on_open, this, ::_1));
+}
+
+void Server::start() {
+    try {
+        server.listen(this->port);
+        server.start_accept();
+        server.run();
+    } catch (const std::exception &e) {
+        std::cout << e.what() << std::endl;
+    } catch (...) {
+        std::cout << "Something horribly went wrong" << std::endl;
+    }
+}
+
+void Server::on_open(websocketpp::connection_hdl hdl) {
+    std::cout << "Connection opened\n";
+
+    std::string path = "/home/hamza/Pictures/385149.jpg"; // Get this path from user
+    std::ifstream ifs(path, std::ios::binary);
+    std::string payload = std::string(std::istreambuf_iterator<char>(ifs), std::istreambuf_iterator<char>());
+    ifs.close();
+
+    server.send(std::move(hdl), payload, websocketpp::frame::opcode::binary);
+}
+
+void Server::on_message(websocketpp::connection_hdl hdl, const asio_server::message_ptr &msg) {
+    try {
+        std::string filename = "name.png"; // temp
+        std::string payload = msg->get_payload();
+        websocketpp::frame::opcode::value opcode = msg->get_opcode();
+
+
+        if (opcode == websocketpp::frame::opcode::binary) {
+            std::cout << filename << "\n";
+            std::cout << "Size " << payload.size() << "\n";
+            std::cout << "opcode " << opcode << "\n";
+
+            std::ofstream outfile;
+            outfile.open(filename);
+            outfile << payload;
+            outfile.close();
+
+            std::cout << "\nBinary received\n";
+
+            server.send(std::move(hdl), "Successfully received", websocketpp::frame::opcode::text);
+        }
+
+    } catch (websocketpp::exception const &e) {
+        std::cout << e.what() << std::endl;
+    }
+}
---
+++ server/includes/headers/server.hpp
@@ -0,0 +1,27 @@
+#ifndef SEND_ANYTHING_SERVER_HPP
+#define SEND_ANYTHING_SERVER_HPP
+
+#include <websocketpp/server.hpp>
+#include <websocketpp/config/asio_no_tls.hpp>
+
+#include <iostream>
+#include <string>
+
+typedef websocketpp::server<websocketpp::config::asio> asio_server;
+
+
+namespace server {
+    class Server {
+    private:
+        asio_server server;
+        int port;
+        void on_open(websocketpp::connection_hdl hdl);
+        void on_message(websocketpp::connection_hdl hdl, const asio_server::message_ptr& msg);
+    public:
+        void start();
+        explicit Server(int _port);
+    };
+
+}
+
+#endif //SEND_ANYTHING_SERVER_HPP
