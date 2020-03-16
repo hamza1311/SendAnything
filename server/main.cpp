4110a7c74bc42de0912e440add354addf949449c
Hamza <muhammadhamza1311@gmail.com>
2020-03-16 20:41:28 +0500

    get gtkmm to work

--- server/main.cpp
+++ server/main.cpp
@@ -1,8 +1,13 @@
-#include "headers/server.hpp"
+//#include "headers/server.hpp"
+#include <gtkmm.h>
 
-int main() {
-    server::Server server(9002);
-    server.start();
+int main(int argc, char *argv[]) {
+//    server::Server server(9002);
+//    server.start();
 
-    return 0;
+    auto app = Gtk::Application::create(argc, argv, "org.gtkmm.examples.base");
+    Gtk::Window window;
+    window.set_default_size(200, 200);
+
+    return app->run(window);
 }
